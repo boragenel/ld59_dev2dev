45437d275cc5a3a41e85c09dfdf9bb1912a43477
andreadbx <67288484+andreadbx@users.noreply.github.com>
2026-04-20 22:24:49 +0200

    Reciever Animation Integration

--- Assets/Scripts/Game/Test/SignalMeshPointReceiver.cs
+++ Assets/Scripts/Game/Test/SignalMeshPointReceiver.cs
@@ -11,16 +11,27 @@ public class SignalMeshPointReceiver : MonoBehaviour {
     //private List<SignalRenderer> signalRenderers = new List<SignalRenderer>();
     public float SignalStrength;
     public bool affectMusic = false;
+    public bool isOn = true;
+    public Animator animator;
 
     public event Action<int> OnSignalStrengthChanged;
 
-    private void LateUpdate() {
+    private void Start()
+    {
+        if (animator == null)
+        {
+            animator = GetComponentInChildren<Animator>();
+        }
+    }
+    private void LateUpdate()
+    {
         Vector3 sample = transform.position + worldOffset;
         SignalMeshFieldManager m = SignalMeshFieldManager.Instance;
-        
-        
+
+
         int next = m != null ? m.CountMeshesContainingWorldPoint(sample) : 0;
-        if (next == SignalStrength && !preciseSignalStrength) {
+        if (next == SignalStrength && !preciseSignalStrength)
+        {
             return;
         }
 
@@ -36,21 +47,46 @@ public class SignalMeshPointReceiver : MonoBehaviour {
             List<Vector3> poses = m != null ? m.GetMeshesContainingWorldPoint(sample) : new List<Vector3>();
             foreach (Vector3 pos in poses)
             {
-                float diff = Mathf.Clamp((pos - transform.position).magnitude,0.25f,8f);
+                float diff = Mathf.Clamp((pos - transform.position).magnitude, 0.25f, 8f);
 
                 SignalStrength += ((8f - diff) / 8f);
             }
-            outcome = Mathf.RoundToInt(SignalStrength*10);
+            outcome = Mathf.RoundToInt(SignalStrength * 10);
 
             if (affectMusic)
             {
                 SoundManager.Instance.masterMixer.GetFloat("MusicLowpass", out float lowpass);
-                SoundManager.Instance.masterMixer.SetFloat("MusicLowpass", Mathf.Lerp(lowpass, 200 + 1000 * SignalStrength,Time.deltaTime*10f));
+                SoundManager.Instance.masterMixer.SetFloat("MusicLowpass", Mathf.Lerp(lowpass, 200 + 1000 * SignalStrength, Time.deltaTime * 10f));
                 GameManager.Instance.uiManager.UpdateReceptionVisuals(SignalStrength);
             }
         }
-        
+
         OnSignalStrengthChanged?.Invoke(outcome);
+        UpdateVisual();
+    }
+
+    private void UpdateVisual()
+    {
+        if (SignalStrength >= 1f)
+        {
+            if (!isOn)
+            {
+                isOn = true;
+                if (animator != null)
+                    animator.SetBool("IsOn", true);
+                Debug.Log("Turned on");
+            }
+        }
+        else
+        {
+            if (isOn)
+            {
+                isOn = false;
+                if (animator != null)
+                    animator.SetBool("IsOn", false);
+                Debug.Log("Turned off");
+            }
+        }
     }
 
     public void ResetSources() {
