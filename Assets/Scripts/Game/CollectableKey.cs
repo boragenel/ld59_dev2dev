032382bb5a9e816e3fb29d43ed1a00d6f0792977
arthursousa <42arthursousa@gmail.com>
2026-04-20 22:02:33 -0300

    key animation fix

--- Assets/Scripts/Game/CollectableKey.cs
+++ Assets/Scripts/Game/CollectableKey.cs
@@ -10,16 +10,16 @@ public class CollectableKey : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             GetComponent<Collider>().enabled = false;
-            SoundManager.Instance.PlayOneShot(SoundType.GATE_KEY_ACQUIRED,0.5f,Random.Range(0.9f,1.1f));
+            SoundManager.Instance.PlayOneShot(SoundType.GATE_KEY_ACQUIRED, 0.5f, Random.Range(0.9f, 1.1f));
             transform.DOBlendableLocalRotateBy(Vector3.one * 720, 1f);
-            transform.DOMove(Vector3.back * 3f, 0.7f).OnComplete(() =>
+            transform.DOMove(GameManager.Instance.currentLevel.levelExit.transform.position + Vector3.back * 3f, 0.7f).OnComplete(() =>
             {
                 transform.DOShakePosition(0.3f, Vector3.one * 0.25f, 30).OnComplete(() =>
                 {
-                    Exit();    
+                    Exit();
                 });
             });
-            
+
         }
     }
 
@@ -32,5 +32,5 @@ public class CollectableKey : MonoBehaviour
         }
         Destroy(gameObject);
     }
-    
+
 }
